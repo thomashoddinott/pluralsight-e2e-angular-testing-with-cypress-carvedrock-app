587e338b9e883a4396bf8f81db6f1a142a877360
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-11-23 08:49:12 +0100

    copy/paste corrected code for new test

--- cypress/integration/mixing_sync_and_async_code.spec.ts
+++ cypress/integration/mixing_sync_and_async_code.spec.ts
@@ -1,19 +1,19 @@
-describe('Mixing synchronous and asynchronous code', () => {
+describe('Mixing synchronous and asyncrhonous code', () => {
   it('should clear a filter if active', () => {
     let isFilterActive = false;
 
-    cy.visit('/')
-    cy.get('input[data-test-automation]="filter"]').type('Tara')
+    cy.visit('/');
+    cy.get('input[data-test-automation="filter"]').type('Tara');
     cy.get('p[data-test-automation="filtered-by"] b').then($el => {
       if ($el.text() === 'Tara') {
-        isFilterActive = true
+        isFilterActive = true;
       }
-    })
 
-    if (isFilterActive) {
-      cy.get('button[data-test-automation="clear-filter"]').click()
-    }
+      if (isFilterActive) {
+        cy.get('button[data-test-automation="clear-filter"]').click();
+      }
+    });
 
-    cy.get('p[data-test-automation="number-of-results"] b').should('have.text', 3)
-  })
-})
+    cy.get('p[data-test-automation="number-of-results"] b').should('have.text', 3);
+  });
+});

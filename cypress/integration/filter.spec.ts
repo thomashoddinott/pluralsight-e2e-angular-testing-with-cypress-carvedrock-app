8ab930343b84285b6d6d188a108f29b50546bf70
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-11-26 16:42:02 +0100

    invoke filter component via `window`
    ...

--- cypress/integration/filter.spec.ts
+++ cypress/integration/filter.spec.ts
@@ -1,5 +1,15 @@
 describe('Filter', () => {
   it('should display filter criterea', () => {
     cy.visit('/')
+    cy.window().then((window: Window) => {
+      cy.wrap(window).its('FilterComponent')
+                     .invoke('onChange', 'Tara')
+      cy.wait(1000)
+      cy.wrap(window).its('appRef')
+                     .invoke('tick')
+    })
+
+    cy.get('p[data-test-automation="filtered-by"')
+      .should('have.text', 'Filtered by: Tara')
   })
-})
+})  
